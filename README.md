007472569944661ad7e25275366eeff0bd2fb39a
Laszlo Lenkei <laszlo.lenkei.ext@siemens-healthineers.com>
2024-02-29 13:51:56 +0100

    4 spaces tab

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim),
+    modified to my taste.
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable LazyVim auto format
+vim.g.autoformat = true
+
+local opt = vim.opt
+opt.tabstop = 4 -- Number of spaces tabs count for
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true

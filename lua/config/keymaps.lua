d6b3f7b7cec0945b1bb5acf83b06faed5aee2d77
Laszlo Lenkei <laszlo.lenkei.ext@siemens-healthineers.com>
2024-11-19 10:22:12 +0100

    arrowkeys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps here-- Resize window using <ctrl> arrow keys
+
+local map = vim.keymap.set
+
+map("n", "<Up>", "3<C-y>", { desc = "Move viewport up" })
+map("n", "<Down>", "3<C-e>", { desc = "Move viewport down" })
+map("n", "<Left>", "20zh", { desc = "Move viewport left" })
+map("n", "<Right>", "20zl", { desc = "Move viewport right" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,4 @@ opt.tabstop = 4 -- Number of spaces tabs count for
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+opt.ignorecase = true
